9142f45a7c9a93fa6c1b3afdf664ded177acc781
Mat Sadler <mat@redstudio.co.uk>
2012-12-03 01:11:39 +0000

    made basic holding page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,18 +2,36 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<script type="text/javascript" src="//use.typekit.net/suh2via.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<title>Welcome to Sinecera Design</title>
 
 	<style type="text/css">
 
 	::selection{ background-color: #E13300; color: white; }
 	::moz-selection{ background-color: #E13300; color: white; }
 	::webkit-selection{ background-color: #E13300; color: white; }
-
+html {
+min-height: 100%;	
+}
 	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		width: 100%;
+	height:100%;
+		background-color: #c9c3b6;
+		background: #e3dcd0; /* Old browsers */
+/* IE9 SVG, needs conditional override of 'filter' to 'none' */
+background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2UzZGNkMCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjU3JSIgc3RvcC1jb2xvcj0iI2M5YzNiNiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNjOWMzYjYiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+background: -moz-linear-gradient(top,  #e3dcd0 0%, #c9c3b6 57%, #c9c3b6 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#e3dcd0), color-stop(57%,#c9c3b6), color-stop(100%,#c9c3b6)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top,  #e3dcd0 0%,#c9c3b6 57%,#c9c3b6 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top,  #e3dcd0 0%,#c9c3b6 57%,#c9c3b6 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top,  #e3dcd0 0%,#c9c3b6 57%,#c9c3b6 100%); /* IE10+ */
+background: linear-gradient(to bottom,  #e3dcd0 0%,#c9c3b6 57%,#c9c3b6 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e3dcd0', endColorstr='#c9c3b6',GradientType=0 ); /* IE6-8 */
+
+		
+		font: 16px/24px normal;
+		font-family: "freight-sans-pro",Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
 
@@ -58,30 +76,28 @@
 	}
 	
 	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
+		margin: 10px 10px 10px 10px;
+		border: 0px solid #D0D0D0;
+		padding-top:200px;
+		
+	}
+	#logo {
+		display:block;
+		width:500px;
+		margin:0 auto 0 auto;
+	}
+	.centerP {
+		margin-top:20px;
+		text-align:center;
 	}
 	</style>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	
+	<img id="logo" src="<?=base_url()?>css/images/logo1000.png" width="500px"/>
+	<div class="centerP">New Website Coming Soon</div>
 </div>
 
 </body>
